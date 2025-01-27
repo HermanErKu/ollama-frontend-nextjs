4079985a8ce3f580a95772f78031b602d05dc9b4
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2025-01-27 18:57:33 +0100

    added a few components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="w-full h-16 bg-gray-800 flex items-center justify-center fixed bottom-0">
+      <h2 className="text-l text-white">©2025 2ITElite</h2>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+
+    return (
+        <header className="w-full h-16 bg-gray-800 flex items-center justify-center">
+            <h1 className="text-white text-xl font-bold">TanVGS GPT</h1>
+        </header>
+    )
+}
+
+export default Header;
